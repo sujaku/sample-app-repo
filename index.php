537bdbc18c6bdbec80b54181059e876c5aec3029
sujaku <sujeetku@hotmail.com>
2016-10-05 01:21:57 +0530

    Added Button to call SPARK API

--- index.php
+++ index.php
@@ -11,9 +11,20 @@
 		font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
 	}
 	</style>
+	<script>
+	function UserAction() {
+    	var xhttp = new XMLHttpRequest();
+    	xhttp.open("POST", "curl https://api.ciscospark.com/v1/messages -X POST -H "Authorization:Bearer ZTg1OWExYmYtODY1My00NjUxLTg5ZTQtZGQ2ZDM5MTM2ZjNkNmIzZGE5YmUtZDA3" --data "toPersonId=722bb271-d7ca-4bce-a9e3-471e4412fa77" --data "text=Hi%20Sparky"", false);
+    	xhttp.setRequestHeader("Content-type", "application/json");
+    	xhttp.send();
+	var response = JSON.parse(xhttp.responseText);
+	}
+	</script>
 </head>
 <body>
 	<img id="logo" src="logo_blue.png" alt="logo.png"/>
 	<h1><?php echo "Hello, World!!"; ?></h1>
+	
+	<button type="submit" value="Say Hi!" onclick="PostSparkMessage()">Search</button>
 </body>
 </html>
